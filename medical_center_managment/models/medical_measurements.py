1d3b699835cad38b10edaa8855b9308b074c5595
hassan <hassan8ghannoum@gmail.com>
2020-05-10 15:50:42 +0300

    update

--- medical_center_managment/models/medical_measurements.py
+++ medical_center_managment/models/medical_measurements.py
@@ -2,6 +2,7 @@ from odoo import api, fields, models, _
 import json
 from matplotlib.patches import Rectangle
 import datetime
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt, mpld3
 from matplotlib.dates import (YEARLY, DateFormatter,
                               rrulewrapper, RRuleLocator, drange)
